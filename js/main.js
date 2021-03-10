894ddd187f27dee52b7ea7da634e5ee3aff312d0
mrmamedzade <cafarmm@code.edu.az>
2021-03-10 19:12:27 +0400

    SliderisDone

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+        items:2,
+        margin:25,
+        loop:true,
+        nav:true,
+         
+    });
+  });
+
+
+  $('.owl-theme').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
