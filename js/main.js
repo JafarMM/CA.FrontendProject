dad8ab5526047a8284ffba51a6e77202dabfd075
mrmamedzade <cafarmm@code.edu.az>
2021-03-14 22:39:21 +0400

    faqspageisdone

--- js/main.js
+++ js/main.js
@@ -28,3 +28,32 @@ $(document).ready(function(){
     }
 })
 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+let navbar = document.querySelector(".navbarm");
+// let firstBadge = document.querySelector(".first-badge");
+// let secondBadge = document.querySelector(".second-badge");
+// let lastBadge = document.querySelector(".last-list-item");
+
+
+window.addEventListener("scroll", function () {
+    navbar.style.transition = "all 0.5s";
+    if (window.pageYOffset > "100") {
+      navbar.style.position = "fixed";
+      navbar.style.width = "100%";
+      navbar.style.top = "0";
+    //   firstBadge.classList.add("first-badge-1");
+    //   secondBadge.classList.add("second-badge-1");
+    //   lastBadge.classList.add("last-list-item-1");
+    } else {
+      navbar.style.position = "";
+    //   firstBadge.classList.remove("first-badge-1");
+    //   secondBadge.classList.remove("second-badge-1");
+    //   lastBadge.classList.remove("last-list-item-1");
+    }
+  });
